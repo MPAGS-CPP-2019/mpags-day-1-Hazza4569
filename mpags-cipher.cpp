4dbee9d73bec06aa0950c7c0ba041564f3f5e050
Harry Cooke <hcooke006@aol.com>
2019-10-11 13:41:42 +0100

    ex4

--- mpags-cipher.cpp
+++ mpags-cipher.cpp
@@ -2,12 +2,24 @@
 #include <string>
 int main()
 {
-    const double const_dbl{6.3};
-    
-    const int my_int{0};
-    my_int += 4;
+    //product of double and int
+    double my_dbl{1.2};
+    int my_int{4};
+    std::cout << my_dbl*my_int << '\n';
+    //outputs 4.8, as expected
 
-    const std::string my_str{"Hello world!"};
+    //int divided by double
+    std::cout << my_int/my_dbl << '\n';
+    //outputs 3.33333, as expected
+
+    //int divided by int
+    int my_int2{3};
+    std::cout << my_int/my_int2 << '\n';
+    //outputs 1 (rather than 1.33333 as an operation on two
+    //int is automatically made an int)
     
-    printf("%.2f %d %s\n", const_dbl, my_int, my_str.c_str());
+    //char from string
+    std::string my_string{"heya"};
+    char my_char{my_string[2]};
+    std::cout << my_char << '\n';
 }
