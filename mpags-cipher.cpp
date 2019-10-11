e031481aa1e613e8c66b7aa85229e21b617567b7
Harry Cooke <hcooke006@aol.com>
2019-10-11 13:20:51 +0100

    main=modifying variables

--- mpags-cipher.cpp
+++ mpags-cipher.cpp
@@ -1,5 +1,13 @@
 #include <iostream>
 int main()
 {
-    std::cout << "Hello World!\n";
+    int my_int{0};
+    my_int += 7.6;
+    
+    double my_dbl{0};
+    my_dbl += 7.6;
+
+    int bad_int{my_dbl};
+
+    printf("My int: %d, my double: %.2f\n", my_int, my_dbl);
 }
