559f878aa4e7385c742284374fad2880244f4229
Harry Cooke <hcooke006@aol.com>
2019-10-18 10:40:52 +0100

    Exercise 5.
    (Note, catching up from last week)

--- mpags-cipher.cpp
+++ mpags-cipher.cpp
@@ -1,25 +1,32 @@
 #include <iostream>
 #include <string>
+
+std::string transliterate(std::string input)
+{
+    std::string strOut = "";
+    //loop over each character
+    for (char iChar : input)
+    {
+        switch (iChar)
+        {
+            case '0': strOut += "ZERO"; break; 
+            case '1': strOut += "ONE"; break; 
+            case '2': strOut += "TWO"; break; 
+            case '3': strOut += "THREE"; break; 
+            case '4': strOut += "FOUR"; break; 
+            case '5': strOut += "FIVE"; break; 
+            case '6': strOut += "SIX"; break; 
+            case '7': strOut += "SEVEN"; break; 
+            case '8': strOut += "EIGHT"; break; 
+            case '9': strOut += "NINE"; break; 
+            default:
+                if ( std::isalpha(iChar) ) strOut += toupper(iChar);
+        } 
+    }
+    return strOut;
+}
+
 int main()
 {
-    //product of double and int
-    double my_dbl{1.2};
-    int my_int{4};
-    std::cout << my_dbl*my_int << '\n';
-    //outputs 4.8, as expected
-
-    //int divided by double
-    std::cout << my_int/my_dbl << '\n';
-    //outputs 3.33333, as expected
-
-    //int divided by int
-    int my_int2{3};
-    std::cout << my_int/my_int2 << '\n';
-    //outputs 1 (rather than 1.33333 as an operation on two
-    //int is automatically made an int)
-    
-    //char from string
-    std::string my_string{"heya"};
-    char my_char{my_string[2]};
-    std::cout << my_char << '\n';
+    printf("%s\n",transliterate("hello12z*'.3j").c_str());
 }
